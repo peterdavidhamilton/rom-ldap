c08eccfb09c059092a1c1e4c5bd429c598d5ee03
Peter David Hamilton <pete@peterdavidhamilton.com>
2017-11-21 07:50:40 +0000

    Prepare fun examples to demonstrate the use of rom-ldap.
    Build an LDIF schema to represent animals and custom attributes and classes.

--- examples/life.rb
+++ examples/life.rb
@@ -1,8 +1,14 @@
 cwd = File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 $LOAD_PATH.unshift(cwd)
 
+# rake ldif\['../../examples/schema'\]
+# rake ldif\['../../examples/animals'\]
+
 require 'pry-byebug'
 require 'rom-ldap'
+
+# Apply a function to coerce all entity attributes into acceptible ruby method names.
+ROM::LDAP::Directory::Entity.to_method_name!
 
 conf = ROM::Configuration.new(
   directory: [
@@ -15,28 +21,81 @@ conf = ROM::Configuration.new(
 conf.relation(:animals, adapter: :ldap) do
   gateway :directory
   base    'dc=example,dc=com'.freeze
-  branches({
-            pets: 'cn=domestic,ou=groups,dc=example,dc=com',
-            zoo:  'cn=wild,ou=groups,dc=example,dc=com'
-          }.freeze)
+  # branches({
+  #           pets: 'cn=domestic,ou=groups,dc=example,dc=com',
+  #           zoo:  'cn=wild,ou=groups,dc=example,dc=com'
+  #         }.freeze)
 
   schema('(species=*)', as: :animals, infer: true)
   use :pagination
   per_page 4
   use :auto_restrictions
+  auto_struct false
 
-  def pets
-    branch(:pets)
+  # def pets
+  #   branch(:pets)
+  # end
+
+  #
+  # Auto Restriction methods
+  #
+  def carnivores
+    by_order('carnivora')
   end
 
-  def zoo
-    branch(:zoo)
+  def great_apes
+    by_family('hominidae')
+  end
+
+  def bears
+    by_family('ursidae')
+  end
+
+
+  def common
+    gte(population_count: 1_000_000)
+  end
+
+  def population_above(num)
+    gte(population_count: num).order(:population_count)
   end
 end
 
 container = ROM.container(conf)
 animals   = container.relations[:animals]
 
-# animals.base('cn=domestic,ou=groups,dc=example,dc=com').count
-# people.zoo.search('(cn=school*)').count
-# people.pets.page(1).search('(cn=*1234*)').first
+#
+# Example Relation Methods
+#
+binding.pry
+
+# grep the entity - matches within arrays
+animals.find(/Mammalia/).select(:cn, :object_class).to_a
+
+# return a single struct
+animals.with(auto_struct: true).where(cn: 'human').one.species
+animals.with(auto_struct: true).equals(cn: 'orangutan').one.cn
+
+# map over multiple structs
+animals.with(auto_struct: true).order(:genus).limit(10).to_a.map(&:cn)
+
+# export to an ldif string
+animals.great_apes.to_ldif
+
+# use an alternative search base
+animals.base('cn=human,ou=animals,dc=example,dc=com').one
+
+# return specific entries
+animals.by_pk('cn=Lion,ou=animals,dc=example,dc=com')
+animals.fetch('cn=Lion,ou=animals,dc=example,dc=com')
+
+
+# animals.zoo.search('(cn=*)').population_count
+# animals.pets.page(1).search('(cn=*house*)').first
+# animals.pets.page(2).pager
+
+# animals.common_birds.to_a
+
+# reveal inferred attributes and coerced types
+animals.schema.to_h
+
