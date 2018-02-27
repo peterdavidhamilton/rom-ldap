cb6dd9bd81efdd36bd8feb4606aa51889fa886d7
Peter David Hamilton <pete@peterdavidhamilton.com>
2018-02-27 17:44:50 +0000

    Refactor reconnection logic with #retry_on_connection_failure block.

--- lib/rom/ldap/connection/delete.rb
+++ lib/rom/ldap/connection/delete.rb
@@ -12,7 +12,6 @@ module ROM
         #
         # @api public
         def delete(dn:, control_codes: nil)
-          connect
           pdu_request = pdu_lookup(:delete_request)
           request     = dn.to_s.to_ber_application_string(pdu_request)
           controls    = control_codes.to_ber_control if control_codes
