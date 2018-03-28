84a36c6078d7712c491ee9d76f0f445bebd7366e
Peter David Hamilton <pete@peterdavidhamilton.com>
2018-03-28 15:57:27 +0100

    Refactor Types to remove namespace for single/multiple values.

--- lib/rom/ldap/types.rb
+++ lib/rom/ldap/types.rb
@@ -14,9 +14,18 @@ module ROM
         ROM::Types.Definition(*args, &block)
       end
 
-      def self.ForeignKey(relation, type = Types::Int.meta(index: true))
-        super
-      end
+      String  = Constructor(::String,    ->(v) { Functions[:stringify].(v[0]) })
+      Int     = Constructor(::Integer,   ->(v) { Functions[:to_int][v][0]  })
+      Symbol  = Constructor(::Symbol,    ->(v) { Functions[:to_sym][v][0]  })
+      Time    = Constructor(::Time,      ->(v) { Functions[:to_time][v][0] })
+      Bool    = Constructor(Types::Bool, ->(v) { Functions[:to_bool][v][0] })
+
+      Strings = Constructor(::String,    Functions[:stringify])
+      Ints    = Constructor(::Integer,   Functions[:to_int])
+      Symbols = Constructor(::Symbol,    Functions[:to_sym])
+      Times   = Constructor(::Time,      Functions[:to_time])
+      Bools   = Constructor(Types::Bool, Functions[:to_bool])
+
 
       UUID_REGEX = /\A
                   [\da-fA-F]{8}-
@@ -24,29 +33,12 @@ module ROM
                   [\da-fA-F]{12}
                   \z/x
 
-      UUID   = Types::Strict::String.constrained(format: UUID_REGEX)
-      Octet  = Types.Constructor(::String, ->(v) { v.force_encoding('UTF-8') })
-      Postal = Types.Constructor(::String, ->(v) { v.split('$') })
-      Serial = Int.meta(primary_key: true)
-      DN     = String.meta(primary_key: true)
-
-      module Single
-        String = Types.Constructor(::String,    ->(v) { v.first.to_s                 })
-        Bool   = Types.Constructor(Types::Bool, ->(v) { Functions[:to_bool][v].first })
-        Int    = Types.Constructor(::Integer,   ->(v) { Functions[:to_int][v].first  })
-        Symbol = Types.Constructor(::Symbol,    ->(v) { Functions[:to_sym][v].first  })
-        Time   = Types.Constructor(::Time,      ->(v) { Functions[:to_time][v].first })
-        Array  = Types::Array
-      end
-
-      module Multiple
-        String = Types::Coercible::Array.member(Types::Coercible::String)
-        Int    = Types::Coercible::Array.member(Types::Coercible::Int)
-        Bool   = Types.Constructor(Types::Bool, ->(v) { Functions[:to_bool][v] })
-        Symbol = Types.Constructor(::Symbol,    ->(v) { v.map(&:to_sym)        })
-        Time   = Types.Constructor(::Time,      ->(v) { Functions[:to_time][v] })
-        Array  = Types::Array
-      end
+      UUID    = Types::Strict::String.constrained(format: UUID_REGEX)
+      Array   = Types::Strict::Array
+      Octet   = Constructor(::String, ->(v) { v.force_encoding('UTF-8') })
+      Postal  = Constructor(::String, ->(v) { v.split('$') })
+      Serial  = Int.meta(primary_key: true)
+      DN      = String.meta(primary_key: true)
     end
   end
 end
