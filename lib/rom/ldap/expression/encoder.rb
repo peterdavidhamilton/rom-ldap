b2681c0cb8355ea9c3e76e699869ea36aa385df2
Peter David Hamilton <pete@peterdavidhamilton.com>
2018-11-06 21:41:32 +0000

    Revert 2.5+ changes for backwards compatibility.

--- lib/rom/ldap/expression/encoder.rb
+++ lib/rom/ldap/expression/encoder.rb
@@ -63,7 +63,7 @@ module ROM
             if right == WILDCARD
               left.to_s.to_ber_contextspecific(7)
 
-            elsif /[*]/.match?(right) # substring
+            elsif /[*]/.match(right) # substring
 
               ary = right.split(/[*]+/, -1)
 
