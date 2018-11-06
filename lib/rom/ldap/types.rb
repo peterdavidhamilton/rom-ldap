8ec5a628200d8716ac70e1fd0f8d15309e4ecf26
Peter David Hamilton <pete@peterdavidhamilton.com>
2018-11-06 19:37:03 +0000

    Rename functions used in type coercions for clarity.

--- lib/rom/ldap/types.rb
+++ lib/rom/ldap/types.rb
@@ -9,19 +9,19 @@ module ROM
       Octet   = Constructor(String,  ->(v) { Functions[:to_hex][v][0] }).meta(octet: true)
       Binary  = Constructor(String,  ->(v) { Functions[:to_binary].(v[0]) }).meta(binary: true)
       String  = Constructor(String,  ->(v) { Functions[:stringify].(v[0]) })
-      Integer = Constructor(Integer, ->(v) { Functions[:to_int][v][0] })
-      Symbol  = Constructor(Symbol,  ->(v) { Functions[:to_sym][v][0] })
-      Time    = Constructor(Time,    ->(v) { Functions[:to_time][v][0] })
-      Bool    = Constructor(Bool,    ->(v) { Functions[:to_bool][v][0] })
+      Integer = Constructor(Integer, ->(v) { Functions[:map_to_integers][v][0] })
+      Symbol  = Constructor(Symbol,  ->(v) { Functions[:map_to_symbols][v][0] })
+      Time    = Constructor(Time,    ->(v) { Functions[:map_to_times][v][0] })
+      Bool    = Constructor(Bool,    ->(v) { Functions[:map_to_booleans][v][0] })
 
       # Multiple Values --------
-      Strings   = Array.constructor(Functions[:stringify])
-      Integers  = Array.constructor(Functions[:to_int])
-      Symbols   = Array.constructor(Functions[:to_sym])
-      Times     = Array.constructor(Functions[:to_time])
-      Bools     = Array.constructor(Functions[:to_bool])
       Octets    = Array.constructor(Functions[:to_hex]).meta(octet: true)
       Binaries  = Array.constructor(Functions[:to_binary]).meta(binary: true)
+      Strings   = Array.constructor(Functions[:stringify])
+      Integers  = Array.constructor(Functions[:map_to_integers])
+      Symbols   = Array.constructor(Functions[:map_to_symbols])
+      Times     = Array.constructor(Functions[:map_to_times])
+      Bools     = Array.constructor(Functions[:map_to_booleans])
 
       # Special LDAP Read Types --------
       Address   = Constructor(String, ->(v) { v.map { |a| a.split('$').map(&:strip) }.first })
