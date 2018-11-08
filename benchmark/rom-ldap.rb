f88a293c6a8dbcd396925b023aafb54ef12f26e4
Peter David Hamilton <pete@peterdavidhamilton.com>
2018-11-08 06:23:58 +0000

    Fix and simplify benchmark using only inferred schema.

--- benchmark/rom-ldap.rb
+++ benchmark/rom-ldap.rb
@@ -1,42 +1,25 @@
 require 'bundler'
+require 'pry'
 
 Bundler.require
 
 ROM::LDAP.load_extensions :compatibility
 
 opts = {
-  username: 'uid=admin,ou=system'
-  password: 'secret'
-  base: 'dc=rom,dc=ldap'
+  username: 'uid=admin,ou=system',
+  password: 'secret',
+  base: 'ou=animals,dc=rom,dc=ldap',
   logger: Logger.new(IO::NULL)
 }
 
-
+filter = '(objectclass=*)'
 
 # ROM-LDAP -------------------------------------
 config = ROM::Configuration.new(:ldap, opts) do |conf|
-  conf.relation(:infer) do
-    schema('(objectclass=person)', infer: true)
-  end
-
-  conf.relation(:explicit) do
-    schema('(objectclass=person)') do
-      attribute :uid,               ROM::LDAP::Types::String
-      attribute :cn,                ROM::LDAP::Types::String
-      attribute :dn,                ROM::LDAP::Types::String
-      attribute :given_name,        ROM::LDAP::Types::String
-      attribute :sn,                ROM::LDAP::Types::String
-      attribute :mail,              ROM::LDAP::Types::String
-      attribute :user_password,     ROM::LDAP::Types::String
-      attribute :uid_number,        ROM::LDAP::Types::Integer
-      attribute :create_timestamp,  ROM::LDAP::Types::Time
-      attribute :object_class,      ROM::LDAP::Types::Strings
-    end
-    auto_struct true
-  end
+  conf.relation(:rom) { schema(filter, infer: true) }
 end
 
-container = ROM.container(config)
+rom = ROM.container(config)
 
 
 # NET-LDAP -------------------------------------
@@ -60,15 +43,11 @@ Benchmark.ips do |bm|
   bm.config(time: 5, warmup: 0.5, iterations: 2)
 
   bm.report('net-ldap') do
-    net_ldap.search(filter: '(objectclass=person)', base: opts[:base], attributes: %w[* +]).to_a
+    net_ldap.search(filter: filter, attributes: %w[* +]).to_a
   end
 
-  bm.report('rom-ldap inferred schema hash') do
-    container.relations[:infer].to_a
-  end
-
-  bm.report('rom-ldap explicit schema struct') do
-    container.relations[:explicit].to_a
+  bm.report('rom-ldap') do
+    rom.relations[:rom].to_a
   end
 
   bm.compare!
