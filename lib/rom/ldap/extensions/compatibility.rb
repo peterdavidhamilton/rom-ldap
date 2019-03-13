c445f05bb27df81f1f069c086baf8fc66948d860
Peter David Hamilton <pete@peterdavidhamilton.com>
2019-03-13 21:10:27 +0000

    Change :compatibility extension to the moved #use_formatter top-level method.

--- lib/rom/ldap/extensions/compatibility.rb
+++ lib/rom/ldap/extensions/compatibility.rb
@@ -1,16 +1,9 @@
 require 'rom/ldap/functions'
-require 'rom/ldap/directory/entry'
 
 module ROM
   module LDAP
     # Assign the formatting proc used to rename the Directory::Entry attributes.
     #
-    # Use the #to_method_name function
-    #
-    formatter_proc = lambda do |key|
-      Functions.to_method_name(key)
-    end
-
-    Directory::Entry.use_formatter(formatter_proc)
+    use_formatter Functions[:to_method_name]
   end
 end
