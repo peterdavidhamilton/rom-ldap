df47d9795719554e8ab880f1c730e177fae472b6
Peter David Hamilton <pete@peterdavidhamilton.com>
2019-03-13 21:14:13 +0000

    Add DSML and MsgPack export extensions.

---
+++ lib/rom/ldap/extensions/exporters/dsml.rb
@@ -0,0 +1,159 @@
+require 'rom/initializer'
+require 'libxml'
+
+# https://publib.boulder.ibm.com/tividd/td/ITIM/SC32-1149-01/en_US/HTML/Policy_Org_Admin395.htm
+
+
+module ROM
+  module LDAP
+
+    #
+    # Directory Service Markup Language (DSML)
+    #
+    # Refines Array and Hash with #to_dsml method.
+    #
+    # @see Directory::Entry
+    # @see Relation::Exporting
+    #
+    module DSML
+      # Export Entry objects as DSML files.
+      #
+      # @param tuple [Entry]
+      #
+      # @api private
+      class Exporter
+        extend Initializer
+
+        include LibXML
+
+
+        # Array of Hash-like objects
+        #
+        param :tuples
+
+        #   <?xml version="1.0" encoding="UTF-8"?>
+        #   <dsml>
+        #     <directory-entries>
+        #       <entry dn="#{dn}">
+        #
+        # @return [String]
+        #
+        # @api private
+        def to_dsml
+          doc = XML::Document.new
+          doc.encoding = XML::Encoding::UTF_8
+          doc.root = root = create_node('dsml')
+          root << (entries = create_node('directory-entries'))
+          map_tuples { |entry| entries << entry }
+          doc.to_s
+        end
+
+        private
+
+        # <entry dn="#{dn}">
+        #
+        # @yield [LibXML::XML::Node]
+        #
+        def map_tuples
+          tuples.each do |tuple|
+            next if tuple.empty?
+
+            dn   = tuple.delete('dn')
+            objc = tuple.delete('objectClass')
+
+            entry_node = create_node('entry', dn: dn&.first)
+
+            classes(objc) { |c| entry_node << c }
+
+            attributes(tuple) { |a| entry_node << a }
+
+            yield(entry_node)
+          end
+        end
+
+        # <oc-value>inetOrgPerson</oc-value>
+        #
+        # @yield [LibXML::XML::Node]
+        #
+        def classes(values)
+          class_node = create_node('objectclass')
+          values.each do |value|
+            value_node = create_node('oc-value')
+            value_node.content = value
+            class_node << value_node
+          end
+          yield(class_node)
+        end
+
+        # @example
+        #
+        #   {'cn'=>['Peter']} => <attr name="cn"><value>Peter</value></attr>
+        #
+        # @yield [LibXML::XML::Node]
+        #
+        def attributes(attrs)
+          attrs.each do |attr_name, attr_values|
+            attr_node = create_node('attr', name: attr_name)
+            attr_values.each do |value|
+              value_node = create_node('value')
+              value_node.content = value
+              attr_node << value_node
+            end
+            yield(attr_node)
+          end
+        end
+
+        #
+        # @return [LibXML::XML::Node]
+        #
+        def create_node(type, params = EMPTY_HASH)
+          node = XML::Node.new(type)
+          unless params.empty?
+            params.each do |key, value|
+              XML::Attr.new(node, key.to_s, value.to_s)
+            end
+          end
+          node
+        end
+      end
+
+      # Extend functionality of Hash class.
+      #
+      refine ::Hash do
+        # Convert hash to DSML format
+        #
+        # @return [String]
+        #
+        # @api public
+        def to_dsml
+          Exporter.new([ self ]).to_dsml
+        end
+      end
+
+      # Extend functionality of Array class.
+      #
+      refine ::Array do
+        # Convert array to DSML format
+        #
+        # @return [String]
+        #
+        # @api public
+        def to_dsml
+          Exporter.new(self).to_dsml
+        end
+      end
+    end
+
+
+    module DSMLExport
+      using DSML
+      #
+      def to_dsml
+        export.to_dsml
+      end
+    end
+
+    Relation.send(:include, DSMLExport)
+
+  end
+end
---
+++ lib/rom/ldap/extensions/exporters/msgpack.rb
@@ -0,0 +1,25 @@
+require 'msgpack'
+
+module ROM
+  module LDAP
+
+    module MsgPackExport
+
+      # Export the relation as MessagePack Binary
+      #
+      # @return [String]
+      #
+      # @example
+      #   relation.to_msgpack
+      #
+      # @api public
+      def to_msgpack
+        export.to_msgpack
+      end
+
+    end
+
+    Relation.send(:include, MsgPackExport)
+
+  end
+end
