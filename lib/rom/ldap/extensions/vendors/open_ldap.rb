aac69f8d20f163ccaaa5ec2548e74a7961d67e04
Peter David Hamilton <pete@peterdavidhamilton.com>
2019-03-13 21:12:22 +0000

    Relocate vendor specific extensions to a subfolder.

--- lib/rom/ldap/extensions/vendors/open_ldap.rb
+++ lib/rom/ldap/extensions/vendors/open_ldap.rb
@@ -1,17 +1,26 @@
-#
-# Directory.include OpenLDAP
-#
 module ROM
   module LDAP
+    #
+    # OpenLDAP
+    #
     module OpenLDAP
+      #
+      # @return [String]
+      #
       def vendor_name
-        'OpenLDAP'
+        'OpenLDAP'.freeze
       end
 
+      #
+      # @return [String]
+      #
       def vendor_version
-        '0.0'
+        '0.0'.freeze
       end
 
+      #
+      # @return [String]
+      #
       def organization
         query(filter: '(objectClass=organization)').first['o'][0]
       end
