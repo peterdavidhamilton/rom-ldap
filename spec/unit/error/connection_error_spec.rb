e978d19d846c56d3fd2e64f6c1824fbdaaf02b7a
Peter David Hamilton <pete@peterdavidhamilton.com>
2019-03-14 10:30:39 +0000

    Uncouple errors from dependencies.

--- spec/unit/error/connection_error_spec.rb
+++ spec/unit/error/connection_error_spec.rb
@@ -2,23 +2,20 @@ RSpec.describe ROM::LDAP::ConnectionError do
 
   include_context 'directory'
 
-  before do
-    conf.relation(:foo) { schema('(dn=*)') }
-  end
-
   context 'correct server' do
     it 'raises no error' do
-      expect { container.relations }.not_to raise_error
+      expect { container }.not_to raise_error
     end
   end
 
   context 'incorrect server' do
-
-    let(:servers) { %w'127.0.0.1:6389' }
+    let(:uri) { 'ldaps://127.0.0.1:6389' }
 
     it 'raises connection error' do
-      expect { container.relations }.
-        to raise_error(ROM::LDAP::ConnectionError, 'Connection failed: 127.0.0.1:6389')
+      expect { container }.to raise_error(
+        ROM::LDAP::ConnectionError,
+        'Connection failed - 127.0.0.1:6389'
+      )
     end
   end
 
