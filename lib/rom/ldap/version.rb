1253939d01d074e1cc9bbfc7feb4d483d9f1ce5b
Peter David Hamilton <pete@peterdavidhamilton.com>
2019-03-15 15:36:45 +0000

    Adopt split version constant used by Sequel gem.

--- lib/rom/ldap/version.rb
+++ lib/rom/ldap/version.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 module ROM
   module LDAP
-    VERSION = '0.1.0'.freeze
+    # The major version of ROM-LDAP. Only bumped for major changes.
+    MAJOR = 0
+
+    # The minor version of ROM-LDAP. Bumped for every non-patch level release.
+    MINOR = 1
+
+    # The tiny version of ROM-LDAP. Only bumped for bugfix releases.
+    TINY  = 0
+
+    # The version of ROM-LDAP, as a string (e.g. "2.11.0")
+    VERSION = [MAJOR, MINOR, TINY].join('.').freeze
   end
 end
