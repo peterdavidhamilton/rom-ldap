f1255f7c15bbaf0542387180872f07bf32dacfd9
Peter David Hamilton <pete@peterdavidhamilton.com>
2019-07-12 10:40:39 +0100

    Improve test for attribute projection and add missing spec for #by_pk.

---
+++ spec/unit/relation/pk_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe ROM::LDAP::Relation, '#by_pk' do
+
+ context 'defaults to DN' do
+    include_context 'people'
+
+    before do
+      factories[:person, cn: 'He-Man', uid_number: 1]
+      factories[:person, cn: 'Skeletor', uid_number: 2]
+    end
+
+
+    it '#by_pk' do
+      expect(people.by_pk('cn=He-Man,ou=specs,dc=rom,dc=ldap').one[:uid_number]).to eql(1)
+      expect(people.with(auto_struct: true).by_pk('cn=Skeletor,ou=specs,dc=rom,dc=ldap').one.cn).to eql(['Skeletor'])
+    end
+  end
+
+
+
+  context 'works with other attributes' do
+    before do
+      conf.relation(:foo) do
+        schema('(objectClass=inetOrgPerson)') do
+          attribute :cn, ROM::LDAP::Types::String
+
+          attribute :uid_number,
+            ROM::LDAP::Types::Integer.meta(primary_key: true),
+            read: ROM::LDAP::Types::Integer
+        end
+      end
+    end
+
+    include_context 'people'
+
+    before do
+      factories[:person, cn: 'Liono', uid_number: 1]
+      factories[:person, cn: 'Snarf', uid_number: 2]
+    end
+
+    let(:relation) { relations[:foo] }
+
+    it 'returns a single tuple' do
+      expect(relation.by_pk(1).one[:uid_number]).to eql(1)
+      expect(relation.by_pk(2).one[:cn]).to eql('Snarf')
+    end
+
+  end
+end
--- spec/unit/relation/project_spec.rb
+++ spec/unit/relation/project_spec.rb
@@ -1,46 +1,112 @@
-RSpec.describe ROM::Relation, '#project / #select' do
+RSpec.describe ROM::Relation do
 
   include_context 'people'
 
   before do
-    10.times.map { 'user' }.each.with_index(1) do |gn, i|
-      factories[:person, uid: "#{gn}#{i}", uid_number: i]
+    factories[:person, cn: 'user', uid_number: 1]
+  end
+
+  describe '#select/#project' do
+
+    context 'with arguments' do
+      let(:relation) { people.select(:cn) }
+
+      it do
+        expect(relation.first).to eql(
+          dn: ['cn=user,ou=specs,dc=rom,dc=ldap'],
+          cn: ['user']
+        )
+      end
+
+      it do
+        expect(relation.with(auto_struct: true).one.to_h).to eql(
+          cn: ['user']
+        )
+      end
     end
+
+    context 'with a block' do
+      it do
+        expect(people.select { [:cn] }.first).to eql(
+          dn: ['cn=user,ou=specs,dc=rom,dc=ldap'],
+          cn: ['user']
+        )
+      end
+
+      it do
+        expect(people.with(auto_struct: true).select { [:cn] }.one.to_h).to eql(
+          cn: ['user']
+        )
+      end
+
+      it do
+        expect(people.select { [cn] }.first).to eql(
+          dn: ['cn=user,ou=specs,dc=rom,dc=ldap'],
+          cn: ['user']
+        )
+      end
+
+      it do
+        expect(people.with(auto_struct: true).select { [cn] }.one.to_h).to eql(
+          cn: ['user']
+        )
+      end
+
+
+      # can only select by the formatted version i.e. snake_case
+      it 'works with aliases' do
+        relation = people.select {
+                                  [
+                                    uid_number.as(:value),
+                                    cn.aliased(:label)
+                                  ]
+                                }
+
+        # aliases ignored
+        expect(relation.first).to eql(
+          cn: ['user'],
+          dn: ['cn=user,ou=specs,dc=rom,dc=ldap'],
+          uid_number: ['1']
+        )
+
+
+        expect(relation.to_a.first).to eql(
+          dn: ['cn=user,ou=specs,dc=rom,dc=ldap'],
+          label: ['user'],
+          value: ['1']
+        )
+
+        #
+        expect(relation.with(auto_struct: true).one.to_h).to eql(
+          label: ['user'], # these change with the schema read types
+          value: ['1']
+        )
+      end
+    end
+
   end
 
-  subject(:relation) { people.with(auto_struct: true).order(:uid) }
 
 
-  it 'arguments' do
-    expect(relation.select(:uid).to_a).to include({ uid: %w'user1' })
-  end
 
-  it 'block' do
-    expect(relation.select { [:uid] }.to_a).to include({ uid: %w'user1' })
-  end
+  describe '#select_append' do
 
-  # it do
-  #   # expect(
-  #     # can only select by the formatted version i.e. snake_case
-  #     # relation.select { [ uidnumber, uid_number.as(:value), cn.aliased(:label) ] }
-  #     relation.select { [ uid_number.aliased(:value), cn.aliased(:label) ] }.to_a
-  #     # ).to include({})
-  # end
+    let(:relation) { people.select(:cn).select_append(:uid_number) }
 
+    it do
+      expect(relation.first).to eql(
+        cn: ['user'],
+        dn: ['cn=user,ou=specs,dc=rom,dc=ldap'],
+        uid_number: ['1']
+      )
+    end
 
-  it '#select_append' do
-    expect(relation.select(:uid).select_append(:uid_number).to_a.first).to be_a(ROM::Struct::Person)
-
-    expect(relation.select(:uid).select_append(:uid_number).to_a.first.to_h).to include({
-      uid: ['user1'],
-      uid_number: 1
-    })
-
-    expect(relation.select(:uid).select_append(:uid_number).first.to_h).to include({
-      uid: ['user1'],
-      uid_number: ['1']
-    })
-
+    it do
+      expect(relation.with(auto_struct: true).one.to_h).to eql(
+        cn: ['user'],
+        uid_number: 1
+      )
+    end
   end
 
 end
